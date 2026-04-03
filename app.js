847ee70c57d8b13563c629628eacb33cbc60da61
jaydeepchavda <jaydipchavda2216@gmail.com>
2026-04-03 18:57:42 +0530

    changes in pipeline versionin

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello DevOps ...');
+  res.end('Hello DevOps 🚀');
 });
 
 server.listen(3000, () => {
   console.log('Server running on port 3000');
+
+  // Stop server after 3 seconds (IMPORTANT)
+  setTimeout(() => {
+    console.log('Stopping server...');
+    process.exit(0);
+  }, 3000);
 });
